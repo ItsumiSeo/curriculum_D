d3f7cfcc8c228aac4e541371b306c2efb3501eca
user.email <choit@LAPTOP-RVEN208A>
2024-04-09 22:43:19 +0900

    課題1.23の提出

---
+++ curriculum1_23/src/curriculum1_23/curriculum1_23.java
@@ -0,0 +1,28 @@
+package curriculum1_23;
+
+public class curriculum1_23 {
+	private String caption = null;
+
+	public static void main(String[] args) {
+		curriculum1_23 a = new curriculum1_23();
+		a.setCaption("""
+				動物名：ライオン
+				体長：2.1m
+				速度：80km/h
+				""");
+		System.out.println(a);
+	}
+		
+		public String getCaption() {
+			return caption;
+		}
+
+		public void setCaption(String caption) {
+			this.caption = caption;
+		}
+		
+		@Override
+		public String toString() {
+			return getCaption();
+		}
+}
